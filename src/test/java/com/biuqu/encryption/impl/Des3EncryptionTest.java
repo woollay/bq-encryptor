b18d69db69139125f6ac8cad2478906fd7d73023
woollay <woollay@gmail.com>
2023-05-23 08:21:21 +0800

    新增对称加密的文档，并修正用例

--- src/test/java/com/biuqu/encryption/impl/Des3EncryptionTest.java
+++ src/test/java/com/biuqu/encryption/impl/Des3EncryptionTest.java
@@ -9,7 +9,6 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -31,16 +30,14 @@ public class Des3EncryptionTest extends BaseSingleEncryptionTest
     }
 
     @Test
-    public void testGetKey() throws NoSuchAlgorithmException
+    public void testGetKey()
     {
         BaseSingleEncryption encryption = new Des3Encryption();
 
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-        byte[] keyBytes = new byte[24];
-        random.nextBytes(keyBytes);
+        byte[] keyBytes = RandomUtils.nextBytes(24);
 
-        //test1: 任意24byte的内容均可以作为3DES的秘钥(而AES/SM4仅支持自己生成的秘钥转换成秘钥对象)
-        SecretKey secretKey = encryption.toKey(keyBytes);
+        //test1: 任意24byte的内容均可以作为3DES的秘钥
+        SecretKey secretKey = encryption.toKey(RandomUtils.nextBytes(24));
         System.out.println("init key=" + Hex.toHexString(keyBytes));
         System.out.println("3des key=" + Hex.toHexString(secretKey.getEncoded()));
         Assert.assertTrue(secretKey.getEncoded().length == 24);
@@ -48,16 +45,12 @@ public class Des3EncryptionTest extends BaseSingleEncryptionTest
         //test2:秘钥对象的二进制和原始秘钥的二进制并不相同
         Assert.assertFalse(Hex.toHexString(secretKey.getEncoded()).equals(Hex.toHexString(keyBytes)));
 
-        byte[] keyBytes2 = new byte[25];
-        random.nextBytes(keyBytes2);
+        byte[] keyBytes2 = RandomUtils.nextBytes(25);
         //test3: 任意大于24byte的内容均可以作为3DES的秘钥，而且只会截取前24byte
         SecretKey secretKey2 = encryption.toKey(keyBytes2);
-        System.out.println("init key2=" + Hex.toHexString(keyBytes2));
-        System.out.println("3des key2=" + Hex.toHexString(secretKey2.getEncoded()));
         Assert.assertTrue(secretKey2.getEncoded().length == 24);
         byte[] subBytes2 = ArrayUtils.subarray(keyBytes2, 0, 24);
-        System.out.println("3des sub key2=" + Hex.toHexString(subBytes2));
-        Assert.assertFalse(Hex.toHexString(secretKey2.getEncoded()).equals(Hex.toHexString(subBytes2)));
+        Assert.assertTrue(Hex.toHexString(secretKey2.getEncoded()).equals(Hex.toHexString(encryption.toKey(subBytes2).getEncoded())));
     }
 
     @Test
@@ -83,19 +76,5 @@ public class Des3EncryptionTest extends BaseSingleEncryptionTest
             BaseSingleEncryption encryption = new Des3Encryption();
             super.doCipher(encryption, len, 8, paddings, modes);
         }
-
-        BaseSingleEncryption encryption = new Des3Encryption();
-        //test2:超长的秘钥和正常长度的秘钥加密效果是一样的
-        byte[] keyBytes2 = RandomUtils.nextBytes(25);
-        byte[] salt = RandomUtils.nextBytes(16);
-        String text = "It's a very very good day...";
-        System.out.println("init key=" + Hex.toHexString(keyBytes2));
-        byte[] encBytes2 = encryption.encrypt(text.getBytes(StandardCharsets.UTF_8), keyBytes2, salt);
-        System.out.println("3des encBytes=" + Hex.toHexString(encBytes2));
-        Assert.assertTrue(encBytes2.length == 32);
-        byte[] keyBytes3 = ArrayUtils.subarray(keyBytes2, 0, 24);
-        byte[] encBytes3 = encryption.encrypt(text.getBytes(StandardCharsets.UTF_8), keyBytes3, salt);
-        Assert.assertTrue(encBytes2.length == 32);
-        Assert.assertArrayEquals(encBytes3, encBytes2);
     }
 }
